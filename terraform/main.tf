a87ec92e2a53a905e537c59d98bcb1db477d342d
Francisco Pitriqueo <franpitri@gmail.com>
2020-11-21 20:12:45 -0300

    🐛 fix: update github secret generation

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,28 +17,28 @@ module "production" {
 }
 
 resource "github_actions_secret" "staging_kubernetes_config" {
-  repository      = "catalogue-microservice"
+  repository      = "kube"
   secret_name     = "STAGING_KUBERNETES_KUBECONFIG"
   plaintext_value = module.staging.kubeconfig
 }
 
 
 resource "github_actions_secret" "staging_public_ip" {
-  repository      = "catalogue-microservice"
+  repository      = "instances"
   secret_name     = "STAGING_PUBLIC_IP"
   plaintext_value = module.staging.public_ip
 }
 
 
 resource "github_actions_secret" "production_kubernetes_config" {
-  repository      = "catalogue-microservice"
+  repository      = "kube"
   secret_name     = "PRODUCTION_KUBERNETES_KUBECONFIG"
   plaintext_value = module.production.kubeconfig
 }
 
 
 resource "github_actions_secret" "production_public_ip" {
-  repository      = "catalogue-microservice"
+  repository      = "instances"
   secret_name     = "PRODUCTION_PUBLIC_IP"
   plaintext_value = module.production.public_ip
 }
