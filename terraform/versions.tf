55051af7c94ec80e97ae4f285f4153505486619e
Francisco Pitriqueo <franpitri@gmail.com>
2020-11-22 16:03:56 -0300

    ✨ feat: add DO spaces

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,4 +1,13 @@
 terraform {
+  backend "s3" {
+    endpoint                    = "nyc3.digitaloceanspaces.com"
+    region                      = "us-west-1"
+    key                         = "terraform.tfstate"
+    skip_requesting_account_id  = true
+    skip_credentials_validation = true
+    skip_get_ec2_platforms      = true
+    skip_metadata_api_check     = true
+  }
   required_providers {
     github = {
       source  = "hashicorp/github"
